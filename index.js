efe8231952223f7bdd78f26fee243e0390df0230
Empereur010 <saidzoungrana04@gmail.com>
2025-06-19 22:45:39 +0000

    Update index.js

--- index.js
+++ index.js
@@ -36,3 +36,12 @@ function startProject() {
 }
 
 startProject();
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("Bot is running"));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Fake web server listening on port ${PORT}`);
+});
